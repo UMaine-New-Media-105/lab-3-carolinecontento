1decc1a4249910e6122b010c2faf34a73264843c
carolinecontento <124187249+carolinecontento@users.noreply.github.com>
2023-02-15 12:20:32 -0500

    changedandaddedex2and3.js
    added exercise 2 and 3 into README

--- README.md
+++
@@ -1,6 +0,0 @@
-# lab-3-carolinecontento
-lab-3-carolinecontento created by GitHub Classroom
-https://editor.p5js.org/carolinecontento/sketches/_sszSYsQA
-abstract shape of a ellispe to rect in the color white
-abstract shape of two rect in the color light green changed based off of mouseX position
-abstract shape of two triangles in the color light pink changed based off of mouseX position
---
+++ READMEEX123.js
@@ -0,0 +1,18 @@
+# lab-3-carolinecontento
+lab-3-carolinecontento created by GitHub Classroom
+https://editor.p5js.org/carolinecontento/sketches/_sszSYsQA
+no stroke for any of the shapes
+abstract shape of a ellispe to rect in the color white
+abstract shape of two rect in the color light green changed based off of mouseX position
+abstract shape of two triangles in the color light pink changed based off of mouseX position
+https://editor.p5js.org/carolinecontento/sketches/slJodRyg6
+function created circleRect 
+size doubles and translated the x and y
+abstract shape of a ellispe to rect in the color white
+abstract shape of two rect in the color light green changed based off of mouseX position
+abstract shape of two triangles in the color light pink changed based off of mouseX position
+https://editor.p5js.org/carolinecontento/sketches/gs-rN4G1v
+created a function 
+abstract shape of a ellispe to rect in the color white
+abstract shape of two rect in the color light green changed based off of mouseX position
+abstract shape of two triangles in the color light pink changed based off of mouseX position
